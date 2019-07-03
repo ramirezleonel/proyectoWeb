f37c519ad6565a751f2ed64171e11de9e19746a2
ramirezleonel <ramirezleonel0@gmail.com>
2019-07-03 10:18:34 -0300

    cambios en las vista login y presupuesto

---
+++ resources/views/login.blade.php
@@ -0,0 +1,62 @@
+<html>
+  <head>
+
+<!-- Bootstrap core CSS -->
+<link href="bootstrap/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom fonts for this template -->
+<link href="bootstrap/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+<link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet'
+  type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+<!-- Custom styles for this template -->
+<link href="bootstrap/css/login.css" rel="stylesheet">
+<!------ Include the above in your HEAD tag ---------->
+  </head>
+<body id="LoginForm">
+<div class="container">
+<br><br><br>
+<div class="login-form">
+<div class="main-div">
+    <div class="panel">
+    <h2>Login</h2>
+    <br>
+    </div>
+    <form id="Login" method="POST">
+        <div class="form-group">
+            <input type="text" class="form-control" id="user" placeholder="Usuario" required>
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" id="password" placeholder="Contraseña" required>
+        </div>
+        <div class="forgot">
+        <a href="reset.html">Recordar Contraseña?</a>
+    </div>
+        <button type="submit" class="btn btn-primary">Ingresar</button>
+
+    </form>
+    </div>
+
+  </div>
+ </div>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="bootstrap/vendor/jquery/jquery.min.js"></script>
+  <script src="bootstrap/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="bootstrap/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Contact form JavaScript -->
+  <script src="bootstrap/js/jqBootstrapValidation.js"></script>
+  <script src="bootstrap/js/contact_me.js"></script>
+
+
+</div>
+
+
+</body>
+</html>
